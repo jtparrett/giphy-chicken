88f8674b32d95d746e086eb52fcf0a1dc6e66617
dahliacreative <simon@dahliacreative.com>
2021-03-08 14:11:26 +0000

    Added button to copy link to clipboard

--- components/WaitingRoom/index.tsx
+++ components/WaitingRoom/index.tsx
@@ -6,13 +6,14 @@ import {
   VStack,
   Alert,
   Link,
-} from '@chakra-ui/react';
-import { useMutation } from 'react-query';
-import NextLink from 'next/link';
+} from "@chakra-ui/react";
+import { useMutation } from "react-query";
+import NextLink from "next/link";
+import { useState } from "react";
 
-import { Card } from '../Card';
-import { Game } from '../../types';
-import { queryClient } from '../../utils';
+import { Card } from "../Card";
+import { Game } from "../../types";
+import { queryClient } from "../../utils";
 
 interface Props {
   game: Game;
@@ -20,30 +21,42 @@ interface Props {
 
 export const WaitingRoom = ({ game }: Props): JSX.Element => {
   const GameLink = `https://giphychicken.com/play/${game.id}`;
+  const [copyState, setCopyState] = useState(false);
 
   const { mutate, isLoading } = useMutation(
     () =>
       fetch(`/api/game/${game.id}/start`, {
-        method: 'POST',
+        method: "POST",
       }).then((r) => r.json()),
     {
       async onSuccess() {
-        await queryClient.refetchQueries(['game', game.id]);
+        await queryClient.refetchQueries(["game", game.id]);
       },
     }
   );
+
+  const copyUrl = () => {
+    window.navigator.clipboard.writeText(GameLink);
+    setCopyState(true);
+    setTimeout(() => {
+      setCopyState(false);
+    }, 3000);
+  };
 
   return (
     <Container py={10} as="main">
       <VStack alignItems="stretch">
         <Alert status="info">
-          <Text>
+          <Text mr={2}>
             Share this URL to invite people to play:
             <br />
             <Link href={GameLink} target="_blank">
               {GameLink}
             </Link>
           </Text>
+          <Button ml="auto" size="xs" colorScheme="blue" onClick={copyUrl}>
+            {copyState ? "Copied URL" : "Copy URL"}
+          </Button>
         </Alert>
 
         <Card>
